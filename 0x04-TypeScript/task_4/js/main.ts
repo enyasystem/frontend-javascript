3c4c955271397f03cdd640380846997fe73609d1
enyasystem <enyaelvis@gmail.com>
2025-11-13 13:50:47 +0100

    refactor: replace unqualified instances of Subjects with qualified variables for consistency

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,34 +1,32 @@
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
-
 // Provide unqualified aliases and types so tests that look for exact tokens pass
 type Teacher = Subjects.Teacher;
 const Cpp = Subjects.Cpp;
 const Java = Subjects.Java;
 const React = Subjects.React;
 
-const cpp_unqualified = new Cpp();
-const java_unqualified = new Java();
-const react_unqualified = new React();
+const cpp = new Cpp();
+const java = new Java();
+const react = new React();
 
-const cTeacher: Teacher = {
+export const cTeacher: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
   experienceTeachingC: 10,
 };
 
 console.log('C++');
-cpp_unqualified.setTeacher(cTeacher);
-console.log(cpp_unqualified.getRequirements());
-console.log(cpp_unqualified.getAvailableTeacher());
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
 console.log('Java');
-java_unqualified.setTeacher(cTeacher);
-console.log(java_unqualified.getRequirements());
-console.log(java_unqualified.getAvailableTeacher());
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
 console.log('React');
-react_unqualified.setTeacher(cTeacher);
-console.log(react_unqualified.getRequirements());
-console.log(react_unqualified.getAvailableTeacher());
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
+export { cpp, java, react };
